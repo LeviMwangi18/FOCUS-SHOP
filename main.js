5497e8cab6e959f147275331d02b1704177943fe
LeviMwangi18 <mungailevi1@gmail.com>
2025-03-02 19:15:23 +0300

    updated

--- main.js
+++ main.js
@@ -43,3 +43,11 @@ function displayProducts(products) {
 }
 // Fetch and display products when the page loads
 fetchProducts();
+
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+
+menuBtn.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+    menuBtn.classList.toggle('active')
+});
